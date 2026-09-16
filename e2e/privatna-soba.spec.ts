ccb1a1e0de67e6ed5df4a86d5f2d3adf39e35d6b
Josip Meštrović <mestrovic000@gmail.com>
2026-09-16 18:49:40 +0200

    Učvrstiti E2E test privatne sobe

--- e2e/privatna-soba.spec.ts
+++ e2e/privatna-soba.spec.ts
@@ -8,6 +8,8 @@ test('dva igrača stvaraju privatnu sobu i pokreću partiju', async ({ browser }
     const gost = igraci[1]!.stranica;
 
     await vlasnik.goto('/soba/kreiraj');
+    await expect(vlasnik.getByRole('heading', { name: 'Nova privatna soba' })).toBeVisible();
+    await vlasnik.waitForFunction(() => performance.getEntriesByType('resource').some((zapis) => zapis.name.includes('/socket.io/')));
     await vlasnik.getByRole('button', { name: 'Stvori privatnu sobu' }).click();
     await expect(vlasnik).toHaveURL(/\/soba\/[A-Z0-9]+$/);
 
